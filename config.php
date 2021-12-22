c186fcb597cf5a48a5a62354a5632fe84413b48f
ABO0000 <albertevistep@gmail.com>
2021-12-22 10:45:36 +0400

    make full

--- config.php
+++ config.php
@@ -46,17 +46,17 @@ class Products
 
     public function connect()
     {
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"], 1);
-        $active_group = 'default';
-        $query_builder = TRUE;
+        // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // $cleardb_server = $cleardb_url["host"];
+        // $cleardb_username = $cleardb_url["user"];
+        // $cleardb_password = $cleardb_url["pass"];
+        // $cleardb_db = substr($cleardb_url["path"], 1);
+        // $active_group = 'default';
+        // $query_builder = TRUE;
+// 
+        // $this->db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-        $this->db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-        // $this->db = new mysqli("127.0.0.1","admin","password","products");
+        $this->db = new mysqli("127.0.0.1","admin","password","products");
 
         return $this->db;
     }
